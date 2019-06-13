0d1475b8defb81b0b9c08958699e3578439336d0
keshav nagpal <keshav.nagpal.1@gmail.com>
2019-06-13 19:04:10 +0530

    Update chatbot_embed.js

--- js/chatbot/chatbot_embed.js
+++ js/chatbot/chatbot_embed.js
@@ -14,7 +14,7 @@ var cookieValidityInHours = 2;
 
 navigator.sayswho = (function () {
     var ua = navigator.userAgent, tem,
-        M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
     if (/trident/i.test(M[1])) {
         tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
         return 'IE ' + (tem[1] || '');
@@ -96,7 +96,7 @@ var markdownIt = window.markdownit();
 console.log("deployement working");
 function AfterUserReceiveMessage(data) {
     //console.log(data);    
-
+    
     $('#' + typingIndicatorId).show();
     if (browser === 'IE') {
         $(chatlogSelector).css({ 'padding-bottom': '20px' });
@@ -109,19 +109,19 @@ function AfterUserReceiveMessage(data) {
         }
         $('#' + typingIndicatorId).hide();
     }, 1000);
-
+    
     // to move the image up in the Hero Card
     $(".ac-image").parent().attr("class", "ac-image-parent");
     $($(".ac-image-parent").siblings()[0]).hide();
     $(".ac-pushButton").css({
         "flex-basis": ""
     });
-
+    
     // To create hover interaction of list of cards
     var checkList = new RegExp('[0-9]+[ ][-][ ]');
     $(".ac-textBlock").each(function () {
         var buttonText = $(this).text();
-
+        
         if (checkList.test(buttonText)) {
             $(this).parent().css({
                 "cursor": "pointer"
@@ -131,12 +131,12 @@ function AfterUserReceiveMessage(data) {
             });
         }
     });
-
+    
     //Disable the url action of title of projects
     $('.ac-textBlock a').click(function (event) {
         event.preventDefault();
         event.stopPropagation();
-
+        
         if ($(this).is('[disabled]')) {
             var that = $(this);
             setTimeout(function () {
@@ -147,7 +147,7 @@ function AfterUserReceiveMessage(data) {
             $(this).parent().trigger('click');
         }
     });
-
+    
     // fix the scrolling issue in webchat ".css-y1c0xs.css-ca0rlf"
     if (scrollControl) {
         var scrollTime = parseInt($(messageListSelector).height() - $(messageListSelector).parent().scrollTop());
@@ -209,117 +209,64 @@ var store = window.WebChat.createStore({}, function (_ref) {
             } else if (action.type === 'DIRECT_LINE/CONNECT_REJECTED') {
                 //resetConversation();
             }
-
+            
             console.log(action.type);
             return next(action);
         };
     };
 });
 
+var styleOptions;
 
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-    try {
-        var info = gen[key](arg);
-        var value = info.value;
-    } catch (error) {
-        reject(error);
-        return;
-    }
+styleOptions = {
+    bubbleFromUserBackground: 'rgba(50, 150, 255, .2)',
+    bubbleBackground: 'rgba(250, 250, 255, .5)',
+    backgroundColor: 'rgba(255, 255, 255, 0.98)',
+    hideUploadButton: true,
+    sendBoxButtonColorOnFocus: 'rgba(0, 0, 0, 1)',
+    sendBoxButtonColorOnHover: 'rgba(0, 0, 100, 1)',
+    suggestedActionHeight: 30,
+    suggestedActionBackground: 'rgba(250, 250, 255, .5)',
+    suggestedActionBorder: "solid 1px ".concat("#0063B1"),
+    suggestedActionBorderRadius: "50px",
+    primaryFont: ["Segoe UI"]
+};
+window.WebChat.renderWebChat({
+    // ngrok http 3978 -host-header="localhost:3978"
+    directLine: directlineObj,
+    // Browser Speech
+    webSpeechPonyfillFactory: window.WebChat.createBrowserWebSpeechPonyfillFactory(),
+    
+    // REPLACE THIS WITH USER ID | passing random id currently to distinguish different users using at same time
+    //userID: "rid" + Math.random().toString().slice(2, 10),
+    userID: userInfo['SDZUserID'],
+    store: store,
+    styleOptions: styleOptions,
+    locale: "en-AU",
+    sendTypingIndicator: false,
+    renderMarkdown: markdownIt.render.bind(markdownIt)
+}, document.getElementById('chat-interface'));
 
-    if (info.done) {
-        resolve(value);
-    } else {
-        Promise.resolve(value).then(_next, _throw);
-    }
-}
+document.querySelector('#chatBotHelp').addEventListener('click', function () {
+    store.dispatch({
+        type: 'WEB_CHAT/SEND_MESSAGE',
+        payload: {
+            text: 'help'
+        }
+    });
+});
 
-function _asyncToGenerator(fn) {
-    return function () {
-        var self = this,
-            args = arguments;
-        return new Promise(function (resolve, reject) {
-            var gen = fn.apply(self, args);
-
-            function _next(value) {
-                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-            }
-
-            function _throw(err) {
-                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-            }
-
-            _next(undefined);
-        });
-    };
-}
-
-_asyncToGenerator(
-    /*#__PURE__*/
-    regeneratorRuntime.mark(function _callee() {
-        var styleOptions;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
-            while (1) {
-                switch (_context.prev = _context.next) {
-                    case 0:
-                        styleOptions = {
-                            bubbleFromUserBackground: 'rgba(50, 150, 255, .2)',
-                            bubbleBackground: 'rgba(250, 250, 255, .5)',
-                            backgroundColor: 'rgba(255, 255, 255, 0.98)',
-                            hideUploadButton: true,
-                            sendBoxButtonColorOnFocus: 'rgba(0, 0, 0, 1)',
-                            sendBoxButtonColorOnHover: 'rgba(0, 0, 100, 1)',
-                            suggestedActionHeight: 30,
-                            suggestedActionBackground: 'rgba(250, 250, 255, .5)',
-                            suggestedActionBorder: "solid 1px ".concat("#0063B1"),
-                            suggestedActionBorderRadius: "50px",
-                            primaryFont: ["Segoe UI"]
-                        };
-                        window.WebChat.renderWebChat({
-                            // ngrok http 3978 -host-header="localhost:3978"
-                            directLine: directlineObj,
-                            // Browser Speech
-                            webSpeechPonyfillFactory: window.WebChat.createBrowserWebSpeechPonyfillFactory(),
-
-                            // REPLACE THIS WITH USER ID | passing random id currently to distinguish different users using at same time
-                            //userID: "rid" + Math.random().toString().slice(2, 10),
-                            userID: userInfo['SDZUserID'],
-                            store: store,
-                            styleOptions: styleOptions,
-                            locale: "en-AU",
-                            sendTypingIndicator: false,
-                            renderMarkdown: markdownIt.render.bind(markdownIt)
-                        }, document.getElementById('chat-interface'));
-
-                        document.querySelector('#chatBotHelp').addEventListener('click', function () {
-                            store.dispatch({
-                                type: 'WEB_CHAT/SEND_MESSAGE',
-                                payload: {
-                                    text: 'help'
-                                }
-                            });
-                        });
-
-                        document.querySelector('#chat-interface > *').focus();
-                        window.addEventListener('webchatoutgoingactivity', function (_ref) {
-                            // You may want to hook to activity of type "event", and based on its "name" and "value" property, you can do further processing.
-                            var data = _ref.data;
-                            AfterUserSendMessage(data);
-                        });
-                        window.addEventListener('webchatincomingactivity', function (_ref) {
-                            var data = _ref.data;
-                            AfterUserReceiveMessage(data);
-                        });
-                        window.addEventListener('webchatconnectionfulfilled', function (_ref) {
-                            var data = _ref.data;
-                            AfterConnectionfulFilled(data);
-                        });
-
-                    case 3:
-                    case "end":
-                        return _context.stop();
-                }
-            }
-        }, _callee, this);
-    }))().catch(function (err) {
-    return console.error(err);
+document.querySelector('#chat-interface > *').focus();
+window.addEventListener('webchatoutgoingactivity', function (_ref) {
+    // You may want to hook to activity of type "event", and based on its "name" and "value" property, you can do further processing.
+    var data = _ref.data;
+    AfterUserSendMessage(data);
+});
+window.addEventListener('webchatincomingactivity', function (_ref) {
+    var data = _ref.data;
+    AfterUserReceiveMessage(data);
+});
+window.addEventListener('webchatconnectionfulfilled', function (_ref) {
+    var data = _ref.data;
+    AfterConnectionfulFilled(data);
 });
